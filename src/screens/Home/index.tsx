d09782bb0df9af223dbf238dd96aad1f2a20b08b
Paulo-Gouvea <paulogouvea1994@gmail.com>
2021-12-07 19:38:39 -0300

    Inicio do projeto

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FlatList } from 'react-native';
+
+import {
+    Container,
+} from './styles';
+
+export function Home(){
+    return (
+        <Container>
+
+        </Container>
+    );
+}
